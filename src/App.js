ff14a53b1ce19e75fe323bfd6eeef4b7ce75d136
Tuscanny Polk <tuscannypolk@gmail.com>
2022-02-21 23:28:30 -0500

    added notes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import images from './images.js';
 
 function App() {
   return (
-   
+  //  this div returns the entire app
   <div className="App">
-  
+  {/* this div holds the moving div to drag */}
  <motion.div className="carousel">
-
+{/* this div is the actual moving div */}
 <motion.div className="inner-carousel">
 {images.map(image => {
 
 return (
 <motion.div className="item">
+  {/* this div takes the images array and makes smaller div's from each image and places them in a img tag.VERY COOL  */}
 <img src={image}></img>
 
 </motion.div>
