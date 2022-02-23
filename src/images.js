418b28228691d7f8e90c5a113475b809278738e9
Tuscanny Polk <tuscannypolk@gmail.com>
2022-02-22 20:46:34 -0500

    updated images and adjusted drag

--- src/images.js
+++ src/images.js
@@ -3,5 +3,6 @@ import image2 from './assets/img2.jpg';
 import image3 from './assets/img3.jpg';
 import image4 from './assets/img4.jpg';
 import image5 from './assets/img5.jpg';
+import image6 from './assets/img6.jpg';
 
-export default [image1, image2, image3, image4, image5];
+export default [image1, image2, image3, image4, image5, image6];
